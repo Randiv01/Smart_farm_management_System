fb9bff5b957241aaf1a274d0825889951a2c8b5c
GimaR03 <it23673922@my.sliit.lk>
2025-08-11 09:26:56 +0530

    Update Navbar.js
    Update Nav bar

--- FRONTEND/src/Components/Navbar/Navbar.js
+++ FRONTEND/src/Components/Navbar/Navbar.js
@@ -20,6 +20,7 @@ const Navbar = () => {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
+            <li><Link to="/HealthManagement">Health Management</Link></li>
             <li><Link to="/AnimalManagement">Animal Management</Link></li>
             <li><Link to="/contact">Contact</Link></li>
             <div className="login-link"><Link to="/login">Login</Link></div>
